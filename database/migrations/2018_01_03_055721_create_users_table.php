5f1a87390e1b9a4b70d65e66545f0a1b6b6ed1f9
NazmulHaqueLimon <nh.limon6600@gmail.com>
2018-01-03 12:31:21 +0600

    first migration as users table created

---
+++ database/migrations/2018_01_03_055721_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->text('api_token');
+            $table->string('name');
+            $table->increments('id');
+            $table->String('email')->unique();
+            $table->string('contact');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
